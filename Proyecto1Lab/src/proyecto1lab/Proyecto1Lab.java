149232e83fbb777670935fc4056c9401b627a77b
alira <aliraquel95@gmail.com>
2026-06-12 14:56:51 -0600

    m

---
+++ Proyecto1Lab/src/proyecto1lab/Proyecto1Lab.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto1lab;
+
+/**
+ *
+ * @author alira
+ */
+public class Proyecto1Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
