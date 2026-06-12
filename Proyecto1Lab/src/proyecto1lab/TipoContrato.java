7e52c3b71893b861c5df25053e7a1b08f7c3ebcf
alira <aliraquel95@gmail.com>
2026-06-12 16:22:51 -0600

    m

---
+++ Proyecto1Lab/src/proyecto1lab/TipoContrato.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package proyecto1lab;
+
+/**
+ *
+ * @author alira
+ */
+public enum TipoContrato {
+    
+    TIEMPO_COMPLETO("Tiempo Completo", 0.30),
+    MEDIO_TIEMPO("Medio Tiempo",0.20),
+    POR_HORA("Por Horas", 0.01),
+    TEMPORAL("Tempora", 0.15);
+    
+    private final String descripcion;
+    private final double factorPrestaciones;
+    
+    TipoContrato(String descripcion, double factorPrestaciones){
+        
+        this.descripcion = descripcion;
+        this.factorPrestaciones = factorPrestaciones;
+    }
+    
+    public String getDescripcion(){
+        return descripcion;
+    }
+    
+    public double getFactorPrestaciones(){
+        
+        return factorPrestaciones;
+    }
+    
+    @Override
+    public String toString() {
+        return descripcion + " (Prestaciones: " + (factorPrestaciones * 100) + "%)";
+    }
+}
