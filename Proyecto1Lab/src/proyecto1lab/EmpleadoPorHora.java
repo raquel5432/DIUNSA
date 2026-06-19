96d8ae792a545b54945f74b637b8154edf8d1763
Joshua Stan <lego24451@gmail.com>
2026-06-19 14:09:17 -0600

    avance 3 sub clases

--- Proyecto1Lab/src/proyecto1lab/EmpleadoPorHora.java
+++ Proyecto1Lab/src/proyecto1lab/EmpleadoPorHora.java
@@ -1,31 +1,37 @@
-
 package proyecto1lab;
 
-public class EmpleadoPorHora extends Empleado{
+public class EmpleadoPorHora extends Empleado {
 
-    private int horas; 
-    private double tarifa; 
-    private int extras; 
-    
-    
-    public EmpleadoPorHora(int horas, double tarifa, int extras){
-        this.horas = horas; 
-        this.tarifa = tarifa; 
-        this.extras = extras; 
+    private double tarifa;
+    private int horasTrabajadas; 
+
+    public EmpleadoPorHora(String id, String nombre, String departamento,
+                           String fechaIngreso, TipoContrato contrato,
+                           double salarioBase, double tarifa, int horasTrabajadas) {
+        super(id, nombre, departamento, fechaIngreso, contrato, salarioBase);
+        this.tarifa          = tarifa;
+        this.horasTrabajadas = horasTrabajadas;
     }
-    
+
     @Override
-    public double calcularPagoQuincenal(){
-        double pago = tarifa * horas; 
-        if(extras > 80 ){
-        double horasExtrasSobreLimite = extras - 80; 
-        pago += horasExtrasSobreLimite * tarifa * 1.5;
-    }
-        return pago; 
+    public double calcularPagoQuincenal() {
+        double pago;
+
+        if (horasTrabajadas <= 80) {
+         
+            pago = horasTrabajadas * tarifa;
+        } else {
+           
+            double horasNormales = 80 * tarifa;
+            double horasExtras   = (horasTrabajadas - 80) * tarifa * 1.5;
+            pago = horasNormales + horasExtras;
+        }
+
+        return pago;
     }
 
     @Override
     public String getCategoria() {
-        return "Por Hora"; 
+        return "Por Hora";
     }
 }
